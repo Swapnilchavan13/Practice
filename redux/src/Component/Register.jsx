d173c398cc0456cd49bf2f83a0636dd0c5f4ab9a
swapnil chavan <chavanswapnil822@gmail.com>
2023-08-09 20:53:08 +0530

    register added

---
+++ redux/src/Component/Register.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegister = () => {
+    // Perform registration logic here
+    console.log('Registering user:', username, password);
+  };
+
+  return (
+    <div className="register-container">
+      <h2>Register</h2>
+      <form>
+        <div className="input-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="button" onClick={handleRegister}>Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
